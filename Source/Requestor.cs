700835c4a97804f733b6a341136b77ce3fad3705
remi <remi@remitaylor.com>
2010-12-06 14:22:10 -0700

    Added Requestor#DefaultHeaders which is simply an IDictionary<string,string> that you can add headers to and they'll be passed along for every request that the Requestor makes.  They can be overriden when calling particular methods.

--- Source/Requestor.cs
+++ Source/Requestor.cs
@@ -41,20 +41,23 @@ namespace Requestoring {
 	// This also helps for inheritance in test environmentst that require static methods, eg. MSpec.
 	public class Static {
 	    public static Requestor Instance = new Requestor();
-	    public static IResponse Get(string path){                 return Instance.Get(path);          }
-	    public static IResponse Get(string path, object vars){    return Instance.Get(path, vars);    }
-	    public static IResponse Post(string path){                return Instance.Post(path);         }
-	    public static IResponse Post(string path, object vars){   return Instance.Post(path, vars);   }
-	    public static IResponse Put(string path){                 return Instance.Put(path);          }
-	    public static IResponse Put(string path, object vars){    return Instance.Put(path, vars);    }
-	    public static IResponse Delete(string path){              return Instance.Delete(path);       }
-	    public static IResponse Delete(string path, object vars){ return Instance.Delete(path, vars); }
-	    public static IResponse FollowRedirect(){                 return Instance.FollowRedirect();   }
-	    public static IResponse LastResponse { get {              return Instance.LastResponse;      }}
-	    public static void      EnableCookies() {                 Instance.EnableCookies();           }
-	    public static void      DisableCookies() {                Instance.DisableCookies();          }
-	    public static void      ResetCookies() {                  Instance.ResetCookies();            }
+	    public static void      EnableCookies() {                 Instance.EnableCookies();              }
+	    public static void      DisableCookies() {                Instance.DisableCookies();             }
+	    public static void      ResetCookies() {                  Instance.ResetCookies();               }
+	    public static IResponse Get(string path){                 return Instance.Get(path);             }
+	    public static IResponse Get(string path, object vars){    return Instance.Get(path, vars);       }
+	    public static IResponse Post(string path){                return Instance.Post(path);            }
+	    public static IResponse Post(string path, object vars){   return Instance.Post(path, vars);      }
+	    public static IResponse Put(string path){                 return Instance.Put(path);             }
+	    public static IResponse Put(string path, object vars){    return Instance.Put(path, vars);       }
+	    public static IResponse Delete(string path){              return Instance.Delete(path);          }
+	    public static IResponse Delete(string path, object vars){ return Instance.Delete(path, vars);    }
+	    public static IResponse FollowRedirect(){                 return Instance.FollowRedirect();      }
+	    public static IResponse LastResponse { get {              return Instance.LastResponse;         }}
+	    public static IDictionary<string,string> DefaultHeaders { get { return Instance.DefaultHeaders; }}
 	}
+
+	public IDictionary<string,string> DefaultHeaders = new Dictionary<string,string>();
 
 	public string RootUrl { get; set; }
 
@@ -84,28 +87,28 @@ namespace Requestoring {
 		return Url(path);
 	}
 
-	public IResponse Get(string path){ return SetLastResponse(Implementation.GetResponse("GET", Url(path), null, null));  }
+	public IResponse Get(string path){ return SetLastResponse(Implementation.GetResponse("GET", Url(path), null, DefaultHeaders));  }
 	public IResponse Get(string path, object variables){
 	    var info = new RequestInfo(variables, "QueryStrings");
-	    return SetLastResponse(Implementation.GetResponse("GET", Url(path, info.QueryStrings), info.PostData, info.Headers));
+	    return SetLastResponse(Implementation.GetResponse("GET", Url(path, info.QueryStrings), info.PostData, MergeWithDefaultHeaders(info.Headers)));
 	}
 
-	public IResponse Post(string path){ return SetLastResponse(Implementation.GetResponse("POST", Url(path), null, null)); }
+	public IResponse Post(string path){ return SetLastResponse(Implementation.GetResponse("POST", Url(path), null, DefaultHeaders)); }
 	public IResponse Post(string path, object variables){
 	    var info = new RequestInfo(variables, "PostData");
-	    return SetLastResponse(Implementation.GetResponse("POST", Url(path, info.QueryStrings), info.PostData, info.Headers));
+	    return SetLastResponse(Implementation.GetResponse("POST", Url(path, info.QueryStrings), info.PostData, MergeWithDefaultHeaders(info.Headers)));
 	}
 
-	public IResponse Put(string path){ return SetLastResponse(Implementation.GetResponse("PUT", Url(path), null, null)); }
+	public IResponse Put(string path){ return SetLastResponse(Implementation.GetResponse("PUT", Url(path), null, DefaultHeaders)); }
 	public IResponse Put(string path, object variables){
 	    var info = new RequestInfo(variables, "PostData");
-	    return SetLastResponse(Implementation.GetResponse("PUT", Url(path, info.QueryStrings), info.PostData, info.Headers));
+	    return SetLastResponse(Implementation.GetResponse("PUT", Url(path, info.QueryStrings), info.PostData, MergeWithDefaultHeaders(info.Headers)));
 	}
 
-	public IResponse Delete(string path){ return SetLastResponse(Implementation.GetResponse("DELETE", Url(path), null, null)); }
+	public IResponse Delete(string path){ return SetLastResponse(Implementation.GetResponse("DELETE", Url(path), null, DefaultHeaders)); }
 	public IResponse Delete(string path, object variables){
 	    var info = new RequestInfo(variables, "PostData");
-	    return SetLastResponse(Implementation.GetResponse("DELETE", Url(path, info.QueryStrings), info.PostData, info.Headers));
+	    return SetLastResponse(Implementation.GetResponse("DELETE", Url(path, info.QueryStrings), info.PostData, MergeWithDefaultHeaders(info.Headers)));
 	}
 
 	IResponse _lastResponse;
@@ -142,6 +145,13 @@ namespace Requestoring {
 	}
 
 	// PRIVATE
+
+	IDictionary<string,string> MergeWithDefaultHeaders(IDictionary<string,string> headers) {
+	    var result = new Dictionary<string,string>(DefaultHeaders);
+	    foreach (var header in headers)
+		result[header.Key] = header.Value;
+	    return result;
+	}
 
 	internal IResponse SetLastResponse(IResponse response) {
 	    _lastResponse = response;
--- Specs/SettingHeadersSpec.cs
+++ Specs/SettingHeadersSpec.cs
@@ -5,10 +5,31 @@ using Requestoring;
 
 namespace Requestoring.Specs {
 
-    [Subject(typeof(Requestor))]
+    [Subject(typeof(Requestor))][SetupForEachSpecification]
     public class SettingHeaders : Spec {
+	Cleanup after_each =()=> DefaultHeaders.Clear();
 
-	It can_set_default_values_for_headers;
+	It can_set_default_values_for_headers =()=> {
+	    Get("/info").Body.ShouldNotContain("Header: CONTENT_TYPE = application/json");
+
+	    DefaultHeaders.Add("Content-Type", "application/json");
+	    Get("/info").Body.ShouldContain("Header: CONTENT_TYPE = application/json");
+
+	    DefaultHeaders.Remove("Content-Type");
+	    Get("/info").Body.ShouldNotContain("Header: CONTENT_TYPE = application/json");
+	};
+
+	It can_override_default_values_for_headers =()=> {
+	    DefaultHeaders.Add("FOO", "bar");
+	    DefaultHeaders.Add("HI",  "there");
+	    Get("/info").Body.ShouldContain("Header: HTTP_FOO = bar");
+	    Get("/info").Body.ShouldContain("Header: HTTP_HI = there");
+	    Get("/info").Body.ShouldNotContain("Header: HTTP_FOO = overriden");
+	    
+	    // HI's default value is still passed along but FOO get overriden
+	    Get("/info", new { Headers = new { FOO="overriden!" }}).Body.ShouldContain("Header: HTTP_FOO = overriden!");
+	    Get("/info", new { Headers = new { FOO="overriden!" }}).Body.ShouldContain("Header: HTTP_HI = there");
+	};
 
 	It can_set_content_type =()=> {
 	    Get("/info"                                                                    ).Body.ShouldNotContain("Header: CONTENT_TYPE = application/json");
